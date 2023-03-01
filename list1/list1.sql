b2af94b467790388c07254a9501535cbab88ee44
xen <selegakamil@gmail.com>
2023-03-01 13:45:25 +0100

    Solve more exercises

--- list1/list1.sql
+++ list1/list1.sql
@@ -9,3 +9,10 @@ SELECT SUBSTRING(CONVERT(EXP(1), CHAR), 2 + 8, 1);
 5.
 SELECT FLOOR(RAND()*6+1);
 6.
+SELECT nazwa, cena*ilosc AS wartosc FROM produkty;
+7.
+SELECT idk, nazwa, CONCAT(miasto, " ", TRIM(SUBSTRING_INDEX(adres, ".", -1))) AS skro
+cony_adres, telefon FROM klienci;
+8.
+SELECT CONCAT_WS(", ", nazwa, adres, miasto, telefon) AS wizytowka FROM klienci;
+9.
