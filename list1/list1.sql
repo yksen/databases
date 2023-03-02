f4fae8221d6f1edfa8172a490b719ef231a381a8
xen <selegakamil@gmail.com>
2023-03-02 19:45:22 +0100

    Fix logic errors in already solved exercises

--- list1/list1.sql
+++ list1/list1.sql
@@ -1,18 +1,17 @@
 1.
-SELECT CURDATE();
+SELECT DAYOFYEAR(CURDATE());
 2.
-SELECT DAYNAME('2001-05-15');
+SELECT WEEKDAY("2001-05-15");
 3.
-SELECT SUBSTRING(CONVERT(PI() + 0.00000000000, CHAR), 2 + 10, 1);
+SELECT RIGHT(CONVERT(TRUNCATE(PI()*1e10, 0), CHAR), 1);
 4.
-SELECT SUBSTRING(CONVERT(EXP(1), CHAR), 2 + 8, 1);
+SELECT RIGHT(CONVERT(TRUNCATE(EXP(1)*1e8, 0), CHAR), 1);
 5.
 SELECT FLOOR(RAND()*6+1);
 6.
 SELECT nazwa, cena*ilosc AS wartosc FROM produkty;
 7.
-SELECT idk, nazwa, CONCAT(miasto, " ", TRIM(SUBSTRING_INDEX(adres, ".", -1))) AS skro
-cony_adres, telefon FROM klienci;
+SELECT idk, nazwa, miasto, CONCAT(TRIM(SUBSTRING_INDEX(adres, ".", -1))) AS skrocony_adres, telefon FROM klienci;
 8.
 SELECT CONCAT_WS(", ", nazwa, adres, miasto, telefon) AS wizytowka FROM klienci;
 9.
