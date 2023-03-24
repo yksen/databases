e62d7a06bea69874927f3f465117a37203dd5a9c
xen <selegakamil@gmail.com>
2023-03-24 22:55:27 +0100

    Solve exercises 11 to 13

--- list3/list3.sql
+++ list3/list3.sql
@@ -15,9 +15,30 @@ JOIN klienci ON zamow.k_id = klienci.idk
 GROUP BY DAYNAME(data)
 ORDER BY ilosc_zamowien DESC;
 5.
-SELECT YEAR(data) AS rok, MONTH(data) AS miesiac, SUM(cena * ilosc) AS wartosc
+SELECT YEAR(data) AS rok, MONTH(data) AS miesiac, SUM(cena * sztuk) AS wartosc
 FROM zamow
 JOIN detal_zamow ON zamow.idz = detal_zamow.z_id
 JOIN produkty ON detal_zamow.p_id = produkty.idp
 GROUP BY YEAR(data), MONTH(data)
 ORDER BY wartosc DESC;
+11.
+SELECT produkty.nazwa, IFNULL(SUM(detal_zamow.sztuk), 0) AS liczba_sprzedanych
+FROM produkty
+LEFT JOIN detal_zamow ON produkty.idp = detal_zamow.p_id
+GROUP BY produkty.nazwa
+ORDER BY liczba_sprzedanych DESC;
+12.
+SELECT klienci.miasto, IFNULL(SUM(detal_zamow.sztuk), 0) AS liczba_zamowionych
+FROM klienci
+LEFT JOIN zamow ON klienci.idk = zamow.k_id
+LEFT JOIN detal_zamow ON zamow.idz = detal_zamow.z_id
+GROUP BY klienci.miasto
+ORDER BY liczba_zamowionych DESC;
+13.
+SELECT klienci.nazwa, klienci.miasto, IFNULL(SUM(produkty.cena * detal_zamow.sztuk), 0) AS wartosc_zamowien
+FROM klienci
+LEFT JOIN zamow ON klienci.idk = zamow.k_id
+LEFT JOIN detal_zamow ON zamow.idz = detal_zamow.z_id
+LEFT JOIN produkty ON detal_zamow.p_id = produkty.idp
+GROUP BY klienci.nazwa, klienci.miasto
+ORDER BY wartosc_zamowien DESC;
