f089eb47947f2cf49c89e68162b275818914bf81
xen <selegakamil@gmail.com>
2023-05-27 01:50:22 +0200

    Fix exercise 1

--- list7/ex1.php
+++ list7/ex1.php
@@ -1,19 +1,15 @@
 <?php
 require_once "database_connection.php";
 
-$query = "
-SELECT data, SUM(cena * sztuk) FROM zamow
-JOIN detal_zamow ON idz = z_id
-JOIN produkty ON idp = p_id
-GROUP BY data
-ORDER BY data";
+$query = "SELECT DATE(data), SUM(cena * sztuk) FROM zamow
+          JOIN detal_zamow ON idz = z_id
+          JOIN produkty ON idp = p_id
+          GROUP BY DATE(data)
+          ORDER BY DATE(data)";
 $result = mysqli_query($database, $query);
 
 echo "<table>";
 echo "<th>Data</th><th>Suma</th>";
 while ($row = $result->fetch_row())
     echo "<tr><td>$row[0]</td><td>$row[1]</td></tr>";
-$result->close();
 echo "</table>";
-
-$database->close();
