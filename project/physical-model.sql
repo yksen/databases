9f7d1d7db18a1adb4c98d9e276d678a2b46aa729
xen <selegakamil@gmail.com>
2023-06-03 20:36:00 +0200

    Add foreign key constraints

--- project/physical-model.sql
+++ project/physical-model.sql
@@ -1,5 +1,3 @@
-SET DEFAULT_STORAGE_ENGINE=INNODB;
-
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
     `id_user` int UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -10,39 +8,7 @@ CREATE TABLE `users` (
     `user_phone` varchar(15) NOT NULL,
     `user_address` varchar(50) NOT NULL,
     PRIMARY KEY (`id_user`)
-) DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `items`;
-CREATE TABLE `items` (
-    `id_item` int UNSIGNED NOT NULL AUTO_INCREMENT,
-    `restaurant_id` int UNSIGNED NOT NULL,
-    `item_name` varchar(50) NOT NULL,
-    `description` varchar(255) NOT NULL,
-    `price` float(10, 2) NOT NULL,
-    PRIMARY KEY (`id_item`),
-    FOREIGN KEY (`restaurant_id`) REFERENCES `restaurants` (`id_restaurant`)
-) DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE `orders` (
-    `id_order` int UNSIGNED NOT NULL AUTO_INCREMENT,
-    `user_id` int UNSIGNED NOT NULL,
-    `date` timestamp NOT NULL,
-    `comment` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id_order`),
-    FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`)
-) DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `order_items`;
-CREATE TABLE `order_items` (
-    `id_order_item` int UNSIGNED NOT NULL AUTO_INCREMENT,
-    `order_id` int UNSIGNED NOT NULL,
-    `item_id` int UNSIGNED NOT NULL,
-    `quantity` int UNSIGNED NOT NULL,
-    PRIMARY KEY (`id_order_item`),
-    FOREIGN KEY (`order_id`) REFERENCES `orders` (`id_order`),
-    FOREIGN KEY (`item_id`) REFERENCES `items` (`id_item`)
-) DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `restaurants`;
 CREATE TABLE `restaurants` (
@@ -54,5 +20,43 @@ CREATE TABLE `restaurants` (
     `restaurant_address` varchar(50) NOT NULL,
     `cuisine` varchar(50) NOT NULL,
     PRIMARY KEY (`id_restaurant`),
-    FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`)
-) DEFAULT CHARSET=utf8;
+    KEY `user_id` (`user_id`),
+    CONSTRAINT `restaurants_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE `items` (
+    `id_item` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `restaurant_id` int UNSIGNED NOT NULL,
+    `item_name` varchar(50) NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `price` float(10, 2) NOT NULL,
+    PRIMARY KEY (`id_item`),
+    KEY `restaurant_id` (`restaurant_id`),
+    CONSTRAINT `items_ibfk_1` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurants` (`id_restaurant`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+    `id_order` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` int UNSIGNED NOT NULL,
+    `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `comment` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id_order`),
+    KEY `user_id` (`user_id`),
+    CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `order_items`;
+CREATE TABLE `order_items` (
+    `id_order_item` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `order_id` int UNSIGNED NOT NULL,
+    `item_id` int UNSIGNED NOT NULL,
+    `quantity` int UNSIGNED NOT NULL,
+    PRIMARY KEY (`id_order_item`),
+    KEY `order_id` (`order_id`),
+    KEY `item_id` (`item_id`),
+    CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id_order`) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id_item`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
